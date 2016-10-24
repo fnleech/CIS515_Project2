f824e5e117a676b616d8bc0dcb2d3d7843dcdd5e
Francine Leech <francinenleech@gmail.com>
2016-10-23 21:31:39 -0400

    problem 1 make w

--- Problem1.m
+++ Problem1.m
@@ -2,6 +2,18 @@
 % Takes as input a vector u and plots the corresponding
 % function plf(u)
 
+%% 1.1 
 yfinal = u;
 xfinal = plf(u); 
+plot(xfinal, yfinal)
+
+%% Concatenate u 8 and 9 times 
+w = [u u u u u u u u];
+yfinal = w;
+xfinal = plf(w); 
+plot(xfinal, yfinal)
+
+w = [u u u u u u u u u];
+yfinal = w;
+xfinal = plf(w); 
 plot(xfinal, yfinal)
