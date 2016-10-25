687b118cb7e71ba64a81aafbd1308844b5641835
Francine Leech <francinenleech@gmail.com>
2016-10-25 15:38:06 -0400

    Problem 2 done

--- Problem2.m
+++ Problem2.m
@@ -1,8 +1,20 @@
 %% Problem 2
 % Test haar.m and haar_inv.m on u (below)
 
+%% 2.1 
 u = [0 2 4 6 6 4 2 1 -1 -2 -4 -6 -6 -4 -2 0];
-
-
 haar_coeff = haar(u); 
 vector = haar_inv(haar_coeff); 
+
+%% 2.2 
+w = [u u u u u u u u];% 8 u's
+haar_coeff = haar(w); 
+vector = haar_inv(haar_coeff); 
+
+w1 = [u u u u u u u u u]; % 9 u's
+haar_coeff = haar(w1); 
+vector = haar_inv(haar_coeff); 
+
+% we observe that w1 is not of dimension 2^n, so we cannot compute the haar
+% coefficient. 
+
--- haar.m
+++ haar.m
@@ -2,20 +2,20 @@
 % The function takes in the vector u and computes the haar transform of a
 % vector using averging and differencing 
 
-function [haar_coeff] =haar(u)
+function [haar_coeff] =haar(w1)
 
-[~, rows] = size(u); 
+[~, rows] = size(w1); 
 
 %Get number of iterations
 n = log2(rows);
 
 %Initial c_old and c_new
-c_new = u; 
+c_new = w1; 
 c_old = c_new; 
 
 %loop through vectors c_old first and then 
 % through i elements in c_new
-for j = n-1:-1:0
+for j = (n-1):-1:0
     for i = 1:(2^j)
     c_old(i) = (c_new((2*i)-1) + c_new(2*i))/2; 
     c_old(2^j+i) = (c_new((2*i)-1) - c_new(2*i))/2;
