d1df7ee1e423e5304328c6554627860125bdb7ca
Francine Leech <francinenleech@gmail.com>
2016-10-25 12:47:13 -0400

    haar_inv function

--- haar_inv.m
+++ haar_inv.m
@@ -1,46 +1,21 @@
 %% Haar Inverse 
 % the reconstruction of a vector from its Haar coefficients
+% function vector = haar_inv(haar_coeff)
+
+
 function vector = haar_inv(haar_coeff)
-
-vector = []; 
-[~, m] = size(haar_coeff); 
-
-%number of rounds of differencing 
-n = m/2;
-
-a = 1; 
-
-for h = 1:n-1 
-    a = 1; % Index for average
-    c = []; 
-    d = [];
-    [~, m] = size(haar_coeff);
-    
-    %STILL NEED TO FIGURE OUT INDICES
-    
-    index = [2 4 8]; 
-    for i = index; 
-        haar_new = haar_coeff(1:i); 
-        [~, n] = size(haar_new);
-        
-        for j = 1:n-1
-        %add
-        c(a) = (haar_new(j) + haar_new(j+1));
-        
-        %substract
-        d(a) = (haar_new(j) - haar_new(j+1));
-        a = a + 1; 
-        end
-        
-    end 
-    
-    %This part still not right
-    if h == n-1;
-    vector = [c d vector];   
-    else 
-    vector = [c d vector]; 
-    end 
-    
-    haar_coeff = [c]; 
+[~, rows] = size(haar_coeff); 
+n = log2(rows);
+u_old = haar_coeff;
+u_new = u_old; 
+for j = 0:n-1
+    for i = 1:(2^j)
+    u_new(2*i-1) = u_old(i) + u_old(2^j + i); 
+    u_new(2*i) = u_old(i) - u_old(2^j + i);
+    end
+    u_old = u_new; 
 end 
+vector = u_old; 
 end 
+
+
