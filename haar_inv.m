1632eca07ce0b824dd2cb76387f3012d74d561ec
Francine Leech <francinenleech@gmail.com>
2016-10-25 12:49:42 -0400

    Add comments to haar_inv

--- haar_inv.m
+++ haar_inv.m
@@ -5,9 +5,16 @@
 
 function vector = haar_inv(haar_coeff)
 [~, rows] = size(haar_coeff); 
+
+%Get number of iterations
 n = log2(rows);
+
+%Initial u_old and u_new
 u_old = haar_coeff;
 u_new = u_old; 
+
+%loop through u_j vectors first and then 
+% through i elements in u_j
 for j = 0:n-1
     for i = 1:(2^j)
     u_new(2*i-1) = u_old(i) + u_old(2^j + i); 
@@ -15,6 +22,7 @@ for j = 0:n-1
     end
     u_old = u_new; 
 end 
+% Get original vector back 
 vector = u_old; 
 end 
 
